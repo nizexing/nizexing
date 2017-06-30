51f71547e3a817d19732a5a69a9b7f7f6ac61e20
dong <18211014203@163.com>
2017-06-30 20:21:27 +0800

    分配路由完毕

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// 后台路由
+// 注册登录
+Route::controller('admin/login','Admin\LonginController');
+// 管理员管理路由
+Route::controller('admin/admin','Admin\AdminController');
+// 用户管理路由
+Route::controller('admin/user','Admin\UserController');
+// 分类管理路由
+Route::controller('admin/type','Admin\TypeController');
+// 视频管理路由
+Route::controller('admin/video','Admin\VideoController');
+// 网站管理路由
+Route::controller('admin/config','Admin\ConfigController');
 
-Route::get('/home/code','CodeController@code');
 
-Route::controller('/home/login','LoginController');
+// 前台路由
+// 首页路由
+Route::controller('/','Home\IndexController');
+// 列表页路由
+Route::controller('/list','Home\ListController');
+// 注册路由
+Route::controller('/reg','Home\RegController');
+// 登录路由
+Route::controller('/login','Home\LoginController');
+// 视频播放页路由
+Route::controller('/play','Home\PlayController');
+// 个人中心路由
+Route::controller('/member','Home\MemberController');
